3e503ee0bfa90394cafcdb439e36dbafba640ce7
Humphrey.... <153418064+hum-phrey7@users.noreply.github.com>
2024-12-14 20:18:26 +0000

    Add files via upload

---
+++ ccc.js
@@ -0,0 +1,9 @@
+
+
+// const bill = 40;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, The tip was ${tip} and the total value ${bill + tip}`);
